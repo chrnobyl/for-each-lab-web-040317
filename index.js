4e85e6c6fe037dbba625679b415f888d892dde77
github username <github email address>
2017-05-15 17:43:53 -0400

    Done w/ spies

---
+++ index.js
@@ -0,0 +1,17 @@
+function iterativeLog(array) {
+  array.forEach(function(element, index, array) {
+    console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(callback) {
+  var arr = [1, 2];
+  arr.forEach(callback)
+    return arr;
+}
+
+function doToArray(array, callback) {
+
+  array.forEach(callback)
+
+}
